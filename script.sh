a40527ce4288e158b3ef6f3f6f74c4b96470d37b
emilletfr <emilletfr@gmail.com>
2016-10-14 18:25:39 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 
 export PATH=/var/lib/jenkins/swift/usr/bin:"${PATH}"
 rm ./.build/debug/App || true
-kill App || true
+pkill App || true
 swift build
 BUILD_ID=dontKillMe nohup ./.build/debug/App >/dev/null 2>1 &
