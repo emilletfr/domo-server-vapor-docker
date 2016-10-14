743c7a96b714e811792cdb556a8d554eb5229745
emilletfr <emilletfr@gmail.com>
2016-10-14 18:44:16 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 
 export PATH=/var/lib/jenkins/swift/usr/bin:"${PATH}"
-rm ./.build/debug/App.build || true
+rm -rf ./.build/debug/App.build || true
 rm ./.build/debug/App.swiftmodule || true
 rm ./.build/debug/App.swiftdoc || true
 pkill App || true
