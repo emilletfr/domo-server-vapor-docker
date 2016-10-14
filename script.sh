f82a607ab6cb661c1443f9d5a6f70367ab359710
emilletfr <emilletfr@gmail.com>
2016-10-14 18:47:21 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,4 @@ rm -rf ./.build/debug/App.build || true
 rm ./.build/debug/App.swiftmodule || true
 rm ./.build/debug/App.swiftdoc || true
 pkill App || true
-swift build
-BUILD_ID=dontKillMe nohup ./.build/debug/App >/dev/null 2>1 &
+
