e52aedcb15cdfb0fda4a59f9a305f03c85246c19
eric <emilletfr@gmail.com>
2016-10-15 10:57:50 +0200

    test

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -18,7 +18,7 @@ var indoorTempController = IndoorTempController(droplet: drop)
 
 
 
-zzDispatchQueue(label: "net.emilletfr.domo.Main.TimerSeconde").async
+DispatchQueue(label: "net.emilletfr.domo.Main.TimerSeconde").async
     {
         while true
         {
