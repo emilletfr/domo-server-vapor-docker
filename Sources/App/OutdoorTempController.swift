3f845f4225d1632f1c1746c07f4ec3faadafd1c6
eric <emilletfr@gmail.com>
2016-10-18 16:47:48 +0200

    commit

--- Sources/App/OutdoorTempController.swift
+++ Sources/App/OutdoorTempController.swift
@@ -15,8 +15,8 @@ import HTTP
 class OutdoorTempController
 {
     let serialQueue = DispatchQueue(label: "net.emilletfr.domo.OutdoorTempManager.Internal")
-    private var internalDegresValue : Double?
-    var degresValue : Double? {
+    private var internalDegresValue : Double = 20.0
+    var degresValue : Double {
         get {return serialQueue.sync { internalDegresValue }}
         set (newValue) {serialQueue.sync { internalDegresValue = newValue}}
     }
@@ -36,8 +36,8 @@ class OutdoorTempController
         }
         
         droplet.get("outdoorTemp") { request in
-            guard let degresValue = self.degresValue else { let res = try Response(status: .badRequest, json:  JSON(node:[])); return res}
-            return String(describing: degresValue)
+          //  guard let degresValue = self.degresValue else { let res = try Response(status: .badRequest, json:  JSON(node:[])); return res}
+            return String(describing: self.degresValue)
         }
     }
     
@@ -49,8 +49,8 @@ class OutdoorTempController
                 {
                     let urlString = "http://api.openweathermap.org/data/2.5/weather?zip=54360,fr&APPID=9c44d7610c061d8c3a7873c51da2e885&units=metric"
                     let response = try self.client.get(urlString)
-                    self.degresValue = response.data["main", "temp"]?.double ?? nil
-                    if let temp = self.degresValue {log( "outdoorTemp : \(temp)")}
+                    self.degresValue = response.data["main", "temp"]?.double ?? 0.0
+                  //  if let temp = self.degresValue {log( "outdoorTemp : \(temp)")}
                 } catch {
                     log(error)
                 }
