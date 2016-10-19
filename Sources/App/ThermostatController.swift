71be5bde1f37346793f351ec56d18c4e1251dba9
eric <emilletfr@gmail.com>
2016-10-19 11:42:27 +0200

    commit

--- Sources/App/ThermostatController.swift
+++ Sources/App/ThermostatController.swift
@@ -15,34 +15,34 @@ import HTTP
 class ThermostatController
 {
     var thermostatTargetTemperature : Double = 20.0
-        /*{
-        get {let value = UserDefaults.standard.double(forKey: "ThermostatTargetTemperature"); return (value < 10.0 ? 10.0 : value) }
-        set (newValue) {UserDefaults.standard.set(newValue, forKey: "ThermostatTargetTemperature")}
-        }*/
- 
+    /*{
+     get {let value = UserDefaults.standard.double(forKey: "ThermostatTargetTemperature"); return (value < 10.0 ? 10.0 : value) }
+     set (newValue) {UserDefaults.standard.set(newValue, forKey: "ThermostatTargetTemperature")}
+     }*/
+    
     var thermostatMode = "auto"
-  //  private var client: ClientProtocol.Type!
-  //  var repeatTimer: DispatchSourceTimer?
+    //  private var client: ClientProtocol.Type!
+    //  var repeatTimer: DispatchSourceTimer?
     //var urlSession : URLSession?
     var indoorTempController : IndoorTempController!
     var outdoorTempController : OutdoorTempController!
-  //  var indoorTemperature : Double = 10.0
- //   var heaterOnOrOffMemory : Bool?
-  //  var pompOnOrOffMemory : Bool?
+    //  var indoorTemperature : Double = 10.0
+    //   var heaterOnOrOffMemory : Bool?
+    //  var pompOnOrOffMemory : Bool?
     var repeatTimerQueue : DispatchQueue?
-
+    
     
     init()
     {
         self.indoorTempController = IndoorTempController()
         self.outdoorTempController = OutdoorTempController()
         /*
-        self.repeatTimer?.cancel()
-        self.repeatTimer = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue(label: "ThermostatController.RepeatTimer"))
-        self.repeatTimer?.scheduleRepeating(deadline: DispatchTime.init(secondsFromNow:1), interval: DispatchTimeInterval.seconds(60))
-        self.repeatTimer?.setEventHandler(handler: self.refresh)
-        self.repeatTimer?.resume()
-        */
+         self.repeatTimer?.cancel()
+         self.repeatTimer = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue(label: "ThermostatController.RepeatTimer"))
+         self.repeatTimer?.scheduleRepeating(deadline: DispatchTime.init(secondsFromNow:1), interval: DispatchTimeInterval.seconds(60))
+         self.repeatTimer?.setEventHandler(handler: self.refresh)
+         self.repeatTimer?.resume()
+         */
         
         self.repeatTimerQueue = DispatchQueue(label: "ThermostatController.Timer")
         self.repeatTimerQueue?.async { [weak self] in
@@ -52,8 +52,6 @@ class ThermostatController
                 self?.refresh()
                 sleep(60)
             }
-            
-
         }
         
         drop.get("thermostat/status") {  request in
@@ -63,7 +61,7 @@ class ThermostatController
                 "humidity": self.indoorTempController.humidityValue,
                 "thermostat": self.thermostatMode
                 ])
-         }
+        }
         
         drop.get("thermostat/targetTemperature", String.self) { request, temperatureString in
             log("targetTemperature : \(temperatureString)")
@@ -76,84 +74,40 @@ class ThermostatController
         drop.get("thermostat", String.self) { request, mode in
             log(mode) // off / comfort / comfort-minus-two / auto
             self.thermostatMode = mode
-           // self.refresh()
             return self.thermostatMode
         }
     }
     
     func refresh()
     {
-   //     DispatchQueue(label: "REFRESH").sync {
         log("ThermostatController:refresh")
-   //     print("thermostatTargetTemperature : \(self.thermostatTargetTemperature)")
-
-            log("indoorTemperature : \(self.indoorTempController.degresValue)° - humidity : \(self.indoorTempController.humidityValue)% - outdoorTemperature : \(Int(self.outdoorTempController.degresValue))°")
+        log("indoorTemperature : \(self.indoorTempController.degresValue)° - humidity : \(self.indoorTempController.humidityValue)% - outdoorTemperature : \(Int(self.outdoorTempController.degresValue))°")
         DispatchQueue.global(qos:.background).async {
             self.forceHeaterOnOrOff(heaterOnOrOff: self.indoorTempController.degresValue < self.thermostatTargetTemperature)
         }
         DispatchQueue.global(qos:.background).async {
             self.forcePompOnOrOff(pompOnOrOff: self.indoorTempController.degresValue < self.thermostatTargetTemperature)
         }
- //       }
-   
     }
     
     func forceHeaterOnOrOff(heaterOnOrOff:Bool)
     {
         do{
-        log("forceHeaterOnOrOff : \((heaterOnOrOff == true ? "1" : "0"))")
-        let urlString = "http://10.0.1.15:8015/0" + (heaterOnOrOff == true ? "1" : "0")
-        _ = try drop.client.get(urlString)
+            log("forceHeaterOnOrOff : \((heaterOnOrOff == true ? "1" : "0"))")
+            let urlString = "http://10.0.1.15:8015/0" + (heaterOnOrOff == true ? "1" : "0")
+            _ = try drop.client.get(urlString)
         }
         catch {log("error : unable to forceHeaterOnOrOff(\((heaterOnOrOff == true ? "1" : "0")))")}
-       // print(response)
-        /*
-        let sessionConfiguration = URLSessionConfiguration.default
-        self.urlSession = URLSession(configuration:sessionConfiguration)
-        self.urlSession?.dataTask(with: URL(string:urlString)!) { (data:Data?, response:URLResponse?, error:Error?) in
-        
-            guard
-            let dataResp = data,
-                let dataString = String(data: dataResp, encoding: .utf8) else {return}
-            print(dataString)
-        }
- */
     }
-    
     
     func forcePompOnOrOff(pompOnOrOff:Bool)
     {
         do
         {
-        log("forcePompOnOrOff : \((pompOnOrOff == true ? "1" : "0"))")
-        let urlString = "http://10.0.1.15:8015/1" + (pompOnOrOff == true ? "1" : "0")
-        _ = try drop.client.get(urlString)
+            log("forcePompOnOrOff : \((pompOnOrOff == true ? "1" : "0"))")
+            let urlString = "http://10.0.1.15:8015/1" + (pompOnOrOff == true ? "1" : "0")
+            _ = try drop.client.get(urlString)
         }
         catch {log("error : unable to forcePompOnOrOff(\((pompOnOrOff == true ? "1" : "0")))")}
-        /*
-        let pompOnOrOffMemoryLocal = pompOnOrOff == true ? 1 : 0
-        if self.pompOnOrOffMemory == -1 || pompOnOrOffMemoryLocal != pompOnOrOffMemory
-        {
-            self.pompOnOrOffMemory = pompOnOrOffMemoryLocal
-        log("forcePompOnOrOff : \((self.heaterOnOrOffMemory))")
-        let urlString = "http://10.0.1.15:8015/1" + (String(self.pompOnOrOffMemory))
-        _ = try? drop.client.get(urlString)
-        }
- */
-      //  print(response)
-        /*
-        let sessionConfiguration = URLSessionConfiguration.default
-        self.urlSession = URLSession(configuration:sessionConfiguration)
-        self.urlSession?.dataTask(with: URL(string:urlString)!) { (data:Data?, response:URLResponse?, error:Error?) in
-            guard
-                let dataResp = data,
-                let dataString = String(data: dataResp, encoding: .utf8) else {return}
-            print(dataString)
-        
-        }
- */
     }
-
-    
-    
 }
