69fe269898495f6dd15c1df79c55db87fb8fa70a
eric <emilletfr@gmail.com>
2016-10-20 13:09:31 +0200

    commit

--- Sources/App/RollerShuttersController.swift
+++ Sources/App/RollerShuttersController.swift
@@ -57,14 +57,12 @@ class RollerShuttersController
     
     func timerSeconde(date:String)
     {
-        /*
         if let sunriseTime = self.sunriseSunsetController.sunriseTime , let sunsetTime = self.sunriseSunsetController.sunsetTime
         {
             log("now : \(date) - sunriseTime : \(sunriseTime) - sunsetTime : \(sunsetTime)")
             if date == "\(sunriseTime):00" {self.actionForAllRollerShutters(openOrClose: true)}
             if date == "\(sunsetTime):00" {self.actionForAllRollerShutters(openOrClose: false)}
         }
- */
     }
     
     func actionForAllRollerShutters(openOrClose:Bool)
