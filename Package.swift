b9e76f746f2f95f0839334d288d8e8b92f924f4e
emilletfr <emilletfr@gmail.com>
2016-12-15 15:56:19 +0100

    Add rxSwift dependency

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/vapor/sqlite-provider", majorVersion: 1, minor: 1)
+        .Package(url: "https://github.com/vapor/sqlite-provider", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3)
 
       ],
     exclude: [
