27e6ca91dcca4e2e80d493fcaa6fbdd2544ca22d
eric <emilletfr@gmail.com>
2016-12-17 16:38:56 +0100

    remove rxSwift from Package

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/vapor/sqlite-provider", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3)
+        .Package(url: "https://github.com/vapor/sqlite-provider", majorVersion: 1, minor: 1)
 
       ],
     exclude: [
