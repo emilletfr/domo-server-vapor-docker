a926c176d18a9809594a18b2b26ceb7caa29eb47
eric <emilletfr@gmail.com>
2017-01-02 17:01:50 +0100

    add Dispatch import in Timers

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,11 +3,21 @@ import HTTP
 import Fluent
 import Foundation
 import Dispatch
+import RxSwift
+import RxBlocking
 
 
 
 let drop = Droplet()
 let internalVarAccessQueue = DispatchQueue(label: "net.emillet.domo.internalVarAccessQueue")
+/*
+var timer: Observable<NSInteger>!
+//create the timer
+timer = Observable<NSInteger>.interval(0.1, scheduler: MainScheduler.instance)
+timer.subscribeNext({ msecs -> Void in
+print("\(msecs)00ms")
+})//.addDisposableTo(bag)
+ */
 
 //let sunriseSunsetController = SunriseSunsetController()
 //_ = ThermostatController()
