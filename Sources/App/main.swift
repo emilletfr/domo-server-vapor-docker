21d2965ef7a6f9c743c2a3ad39c78b78a976d3c5
eric <emilletfr@gmail.com>
2017-01-06 13:52:41 +0100

    Give another try to Timer

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -26,8 +26,6 @@ DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + .seconds
 DispatchQueue.global(qos: .userInitiated).async{
     print("asyncAfter finished")
     for i in 1...1000 {print("D")}
-    
-    
 }
 print("MainThread")
  for i in 1...1000 {print("M")}
