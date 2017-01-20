6f9b6a50019fb313e0b0cf04a97b27a63b11dd40
eric <emilletfr@gmail.com>
2017-01-20 06:17:44 +0100

    beta 4

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,7 +6,7 @@ let drop = Droplet()
 let thermostatViewController = ThermostatViewController()
 let rollerShuttersViewController = RollerShuttersViewController()
 
-//drop.log.enabled =  [.error, .fatal]
+drop.log.enabled =  [.error, .fatal]
 drop.run()
 
 
