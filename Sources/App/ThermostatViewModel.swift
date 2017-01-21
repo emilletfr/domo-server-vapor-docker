dc3f181aeeb8e5c18f945ae25033545f9020560e
eric <emilletfr@gmail.com>
2017-01-21 15:49:51 +0100

    try to prevent high cpu

--- Sources/App/ThermostatViewModel.swift
+++ Sources/App/ThermostatViewModel.swift
@@ -92,7 +92,7 @@ final class ThermostatViewModel : ThermostatViewModelable
         let heatingOrCoolingReducer = Observable<Bool>.combineLatest(indoorTempReducer, targetTempReducer) {$0 < Double($1)}
         
         // Activate Boiler
-        _ = heatingOrCoolingReducer
+        _ = heatingOrCoolingReducer.debug()
             .throttle(60, scheduler: ConcurrentDispatchQueueScheduler(qos: .default))
             .subscribe(onNext:
                 { (heatingOrCooling:Bool) in
