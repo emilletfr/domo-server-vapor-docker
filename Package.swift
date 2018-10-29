7e3d2e9b6d0e1453dc9424f93716072cb16cfd57
Unknown <emilletfr@gmail.com>
2018-10-29 10:11:54 +0100

    init new vapor project from api template and add rxswift dependency

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "domo-server-vapor-docker",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0" ..< "5.0.0")
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "RxSwift"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
+
