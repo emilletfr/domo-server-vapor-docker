91b7fa5a08f98f9966dac267059a53d70861539a
Unknown <emilletfr@gmail.com>
2018-10-30 17:22:17 +0100

    add 1 sec timer and make app global

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,6 +1,13 @@
 import Vapor
 
+
+var app: Application!
+
 /// Called after your application has initialized.
-public func boot(_ app: Application) throws {
+public func boot(_ application: Application) throws {
     // your code here
+    app = application
+    app.eventLoop.scheduleRepeatedTask(initialDelay: TimeAmount.seconds(0), delay: TimeAmount.seconds(1)) { (task:RepeatedTask) in
+        print("\(Date().timeIntervalSince1970)")
+    }
 }
